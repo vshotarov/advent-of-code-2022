351b75a8e83505d0f84af35363d69fece256a992
Vasil Shotarov <vshotarov@gmail.com>
2022-12-17 06:08:20 +0000

    cleaned up day 11

--- day11/Main.hs
+++ day11/Main.hs
@@ -2,10 +2,7 @@ module Main where
 
 import qualified Common
 import Data.List.Split (splitOn)
-import Data.List (transpose)
 import Data.List (sort)
-
-import Debug.Trace
 
 main :: IO ()
 main = do
@@ -17,19 +14,14 @@ main = do
     putStrLn $ "Parsed input: " ++ (Common.truncateString $ show parsedInput)
 
     -- Solve
-    let after20Rounds = take 21
-                      $ iterate playRound parsedInput
-            where playRound monkeys = foldl (\acc x -> playOneMonkey x acc)
+    let playNRounds n worryFunc = take (n+1) $ iterate playRound parsedInput
+            where playRound monkeys = foldl (\acc x -> playOneMonkey worryFunc x acc)
                                             monkeys [0..((length monkeys) - 1)]
-    let testsProduct = foldr1 (*) $ map mtest parsedInput
-    let after20Rounds' = take 10001
-                      $ iterate playRound parsedInput
-            where playRound monkeys = foldl (\acc x -> playOneMonkey' testsProduct x acc)
-                                            monkeys [0..((length monkeys) - 1)]
-    let answer1 = foldr1 (*) . take 2 . reverse . sort . map mnumActions
-                $ last after20Rounds
-    let answer2 = foldr1 (*) . take 2 . reverse . sort . map mnumActions
-                $ last after20Rounds'
+    let monkeyBusiness = foldr1 (*) . take 2 . reverse . sort . map mnumActions
+    let answer1 = monkeyBusiness . last $ playNRounds 20 (\x -> x `div` 3)
+    let answer2 = monkeyBusiness . last $ playNRounds 10000 (\x -> x `mod` testsProduct)
+            where testsProduct = foldr1 (*) $ map mtest parsedInput
+
 
     -- Print answers
     putStrLn $ "Part 1: " ++ show answer1
@@ -44,60 +36,44 @@ instance Show Monkey where
 
 parse :: String -> [Monkey]
 parse input = map parseMonkey . splitOn [""] $ lines input
-    where parseMonkey monkey = let [id,items,op,test,succ,fail] = monkey
-                                   id' = read (drop 7 $ init id)
+    where parseMonkey monkey = let (_id:items:op:test:_succ:_fail:_) = monkey
+                                   id' = read (drop 7 $ init _id)
                                    items' = map read . splitOn ", "
                                           . last $ splitOn ": " items
                                    op' = snd $ Common.splitOnceOn "= " op
                                    op'' = case splitOn " " op' of
-                                            [_,"+","old"] -> (\x -> x + x)
                                             [_,"*","old"] -> (\x -> x * x)
                                             [_,"+",val] -> (\x -> x + (read val))
                                             [_,"*",val] -> (\x -> x * (read val))
                                             x -> error ("Unrecognised op: " ++ show x)
                                    test' = (read . last $ splitOn "by " test)
-                                   succ' = (read . last $ splitOn "monkey " succ)
-                                   fail' = (read . last $ splitOn "monkey " fail)
-                                in Monkey id' items' op'' test' succ' fail' 0
+                                   _succ' = (read . last $ splitOn "monkey " _succ)
+                                   _fail' = (read . last $ splitOn "monkey " _fail)
+                                in Monkey id' items' op'' test' _succ' _fail' 0
 
-playOneMonkey :: Int -> [Monkey] -> [Monkey]
-playOneMonkey i monkeys = let m = monkeys !! i
-                              items = mitems m
-                              items' = map ((`div` 3) . (mop m)) items
-                              successes = filter ((==0) . (`mod` (mtest m))) items'
-                              failures = filter (not . (`elem` successes)) items'
-                              successM = monkeys !! (msuccess m)
-                              failureM = monkeys !! (mfail m)
-                              successM' = addItems successM successes
-                              failureM' = addItems failureM failures
-                              monkeys' = take (msuccess m) monkeys ++ (successM':(drop (msuccess m + 1) monkeys))
-                              monkeys'' = take (mfail m) monkeys' ++ (failureM':(drop (mfail m + 1) monkeys'))
-                              m' = clearItems m
-                              monkeys''' = take i monkeys'' ++ (m':(drop (i+1) monkeys''))
-                           in monkeys'''
-
-playOneMonkey' :: Int -> Int -> [Monkey] -> [Monkey]
-playOneMonkey' x i monkeys = let m = monkeys !! i
-                                 items = mitems m
-                                 items' = map ((`mod` x) . (mop m)) items
-                                 successes = filter ((==0) . (`mod` (mtest m))) items'
-                                 failures = filter (not . (`elem` successes)) items'
-                                 successM = monkeys !! (msuccess m)
-                                 failureM = monkeys !! (mfail m)
-                                 successM' = addItems successM successes
-                                 failureM' = addItems failureM failures
-                                 monkeys' = take (msuccess m) monkeys ++ (successM':(drop (msuccess m + 1) monkeys))
-                                 monkeys'' = take (mfail m) monkeys' ++ (failureM':(drop (mfail m + 1) monkeys'))
-                                 m' = clearItems m
-                                 monkeys''' = take i monkeys'' ++ (m':(drop (i+1) monkeys''))
-                              in monkeys'''
+playOneMonkey :: (Int -> Int) -> Int -> [Monkey] -> [Monkey]
+playOneMonkey worryFunc i monkeys =
+    let m = monkeys !! i
+        items = mitems m
+        items' = map (worryFunc . (mop m)) items
+        successes = filter ((==0) . (`mod` (mtest m))) items'
+        failures = filter (not . (`elem` successes)) items'
+        successM = monkeys !! (msuccess m)
+        failureM = monkeys !! (mfail m)
+        successM' = addItems successM successes
+        failureM' = addItems failureM failures
+        monkeys' = take (msuccess m) monkeys ++ (successM':(drop (msuccess m + 1) monkeys))
+        monkeys'' = take (mfail m) monkeys' ++ (failureM':(drop (mfail m + 1) monkeys'))
+        m' = clearItems m
+        monkeys''' = take i monkeys'' ++ (m':(drop (i+1) monkeys''))
+     in monkeys'''
 
 addItems :: Monkey -> [Int] -> Monkey
-addItems (Monkey {mid=id, mitems=items, mop=op, mtest=test,
-                  msuccess=succ, mfail=fail, mnumActions=numActions}) newItems =
-    Monkey id (items ++ newItems) op test succ fail numActions
+addItems (Monkey {mid=_id, mitems=items, mop=op, mtest=test,
+                  msuccess=_succ, mfail=_fail, mnumActions=numActions}) newItems =
+    Monkey _id (items ++ newItems) op test _succ _fail numActions
 
 clearItems :: Monkey -> Monkey
-clearItems (Monkey {mid=id, mitems=items, mop=op, mtest=test,
-                    msuccess=succ, mfail=fail, mnumActions=numActions}) =
-    Monkey id [] op test succ fail (numActions + length items)
+clearItems (Monkey {mid=_id, mitems=items, mop=op, mtest=test,
+                    msuccess=_succ, mfail=_fail, mnumActions=numActions}) =
+    Monkey _id [] op test _succ _fail (numActions + length items)
